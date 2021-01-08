327e976cf432eda63928edb8922b2ec80acf1aa2
Sidhant Bansal <sidhantbansal@Sidhants-MacBook-Pro-2.local>
2021-01-08 15:12:07 +0800

    Add tracking of last movement

--- content.js
+++ content.js
@@ -2,17 +2,32 @@ console.log("Hello World");
 var x = null;
 var y = null;
 document.addEventListener("mousemove", onMouseUpdate, false);
+document.addEventListener("mousewheel", onMouseUpdate, false);
 
-var queue = new Array(20);
+var queue = [];
 
 setInterval(function () {
-  // console.log("x= " + x + "y = " + y);
-  if (queue.length >= 20) {
-    queue.shift()
-  }
-  queue.push([x, y])
-  console.log(queue)
-}, 50);
+    // console.log("x= " + x + "y = " + y);
+    if (queue.length >= 20) {
+        queue.shift()
+    }
+    queue.push([x, y])
+    if(queue.length > 0) {
+        startX = queue[0][0];
+        startY = queue[0][1];
+        endX = queue[queue.length - 1][0]
+        endY = queue[queue.length - 1][1]
+        arrow = document.createElement('img');
+        document.body.appendChild(arrow);
+        arrow.src = "https://upload.wikimedia.org/wikipedia/commons/e/e6/Red_rectangle.svg";
+        arrow.style.position = "fixed";
+        arrow.style.top = startY + 'px';
+        arrow.style.left = startX + 'px';
+        arrow.style.width = '10px';
+        arrow.style.height = '10px';
+        console.log(queue[0])
+    }
+}, 10);
 
 function onMouseUpdate(e) {
     x = e.pageX;
@@ -45,7 +60,6 @@ function show_rectangle(rect) {
     } else {
         rect.classList.add('animated_bold');
     }
-    
 }
 
 function show_rectangles(rects) {
