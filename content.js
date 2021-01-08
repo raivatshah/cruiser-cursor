17d81721287ea7f13d0ecfc33792c5f296565f33
Sidhant Bansal <sidhantbansal@Sidhants-MacBook-Pro-2.local>
2021-01-08 11:34:12 +0800

    Starter code

---
+++ content.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+var x = null;
+var y = null;
+document.addEventListener('mousemove', onMouseUpdate, false);
+
+function onMouseUpdate(e) {
+    x = e.pageX;
+    y = e.pageY;
+    console.log(x, y);
+}
+
+// https://stackoverflow.com/questions/2601097/how-to-get-the-mouse-position-without-events-without-moving-the-mouse
